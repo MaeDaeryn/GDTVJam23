36835f344440c0a940297271a294d45f900141e7
GreyDaeryn <120057717+GreyDaeryn@users.noreply.github.com>
2023-05-25 22:32:32 +0200

    P&C Rraycast+Collider 2505

--- GDTV23/Assets/Scripts/P&C/AdvenureScript.cs
+++ GDTV23/Assets/Scripts/P&C/AdvenureScript.cs
@@ -9,6 +9,8 @@ public class NewBehaviourScript : MonoBehaviour
     public Vector3 mousePos;
     public Camera mainCam;
     public Vector3 mouseWorld;
+    public Vector2 mouseWorl2D;
+    public RaycastHit2D hit;
 
     // Start is called before the first frame update
     void Start()
@@ -31,6 +33,22 @@ public class NewBehaviourScript : MonoBehaviour
             mouseWorld = mainCam.ScreenToWorldPoint(mousePos);
 
             print("World = " + mouseWorld);
+
+            mouseWorl2D = new Vector2(mouseWorld.x, mouseWorld.y);
+
+            //Raycast2D
+            hit = Physics2D.Raycast(mouseWorl2D, Vector2.zero);
+
+            if(hit.collider != null)
+            {
+                print("Collider getroffen ");
+
+                print(hit.collider.gameObject.name);
+            }
+            else
+            {
+                print("Kein Collider ");
+            }
         }
     }
 }
